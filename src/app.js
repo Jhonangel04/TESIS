f251f7fcf2427b71c9d0138259b8640f97e6ad56
Jhonangel Briceño <jhonangelbriceo04@gmail.com>
2024-11-20 11:12:17 -0400

    añadiendo morgan

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express"
 import bodyParser from "body-parser"
+import morgan from 'morgan';
+
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send(`Hello !`);
