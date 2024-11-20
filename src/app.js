a4a2768b97530f5adf1c7f47bb54ac12b718f7cb
Jhonangel Briceño <jhonangelbriceo04@gmail.com>
2024-11-20 10:34:25 -0400

    cambios carpeta SRC

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+import bodyParser from "body-parser"
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send(`Hello !`);
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-import express from "express"
-import bodyParser from "body-parser"
-const app = express();
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-    const data = req.body;
-    res.send(`Hello !`);
-});
+import app from "./app.js";
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
