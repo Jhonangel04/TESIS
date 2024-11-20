2d7af5c04b6668960403ac0c80084daf8a021d1a
Jhonangel Briceño <jhonangelbriceo04@gmail.com>
2024-11-20 14:02:13 -0400

    añadiendo ruta y controlador register y login

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";12
+import bodyParser from "body-parser";12
 import morgan from 'morgan';
+
+import authRoutes from "./routes/auth.router.js";
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use('/api', authRoutes);
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send(`Hello !`);
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const login = (req, res) => {
+    res.json("hola login")
+}
+
+const register = (req, res) => {
+    res.json("hola register")
+}
+
+export {
+    login,
+    register
+}
+
