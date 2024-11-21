98a2e4b78106822036d7476340428bfa1172e6fc
Jhonangel Briceño <jhonangelbriceo04@gmail.com>
2024-11-21 10:38:25 -0400

    modificación app

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use('/api', authRoutes);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
