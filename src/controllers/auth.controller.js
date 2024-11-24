f2c4967e7658643d25dccf95dd9dd7ad04697eaa
Jhonangel Briceño <jhonangelbriceo04@gmail.com>
2024-11-24 14:41:28 -0400

    todo

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { pool } from "../db.js";
+import bcrypt from "bcryptjs"
 
 const register = async (req, res) => {
   try {
@@ -10,9 +11,19 @@ const register = async (req, res) => {
     const RoleID = getDefaultRoleID.rows[0].id; 
 
 
-    const {Nombre, apellido, identificación, correo_electrónico, contraseña, fecha_de_nacimiento} = req.body;
+    const {name, lastname, identification, email, password, date_of_birth } = req.body;
 
-    
+    if (!name || !lastname || !identification || !email ||!password || !date_of_birth ) return res.json({ message: "required fields" });
+
+    if (email)
+      return res.status(400).json({ message: ["The Email already exists"] });
+
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const createUser = await client.query('INSERT INTO user (name, lastname, identification, email, password, role_id, date_of_birth) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [name, lastname, identification, email, password, RoleID ,date_of_birth]);
+
+
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Database error" });
